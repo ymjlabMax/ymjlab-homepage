8e9249556d178697860a66099cddad6190bd7237
Max <sud665@ymjlab.com>
2023-01-18 16:32:46 +0900

    초기 세팅

---
+++ 4요망진연구소/ymjlabhomepage/homepage/src/pages/about/kkultrippage.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+    return (
+        <>
+            <Link href="/">
+                <h1>ddddd333333</h1>
+            </Link>
+        </>
+    );
+}
