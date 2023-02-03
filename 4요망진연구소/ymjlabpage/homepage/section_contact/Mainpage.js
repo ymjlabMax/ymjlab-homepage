4708c74ae3f3fc4efe1115ed84eda842b14ad459
ymjlabMax <sud665@ymjlab.com>
2023-02-03 17:16:20 +0900

    contact 페이지 추가

--- 4요망진연구소/ymjlabpage/homepage/section_contact/Mainpage.js
+++ 4요망진연구소/ymjlabpage/homepage/section_contact/Mainpage.js
@@ -15,62 +15,162 @@ export default function ContactPage({ children }) {
 
     return (
         <section>
-            <div className="title">
-                <h1>Contact</h1>
-            </div>
-            <form className="pc-form">
-                <div>
+            <div className="contact-wrap">
+                <div className="left-box">
+                    <div className="title">
+                        <h1>Contact</h1>
+                    </div>
+                    <div className="sub-title">
+                        <h4>요망진연구소는 다양한 분야에서 기업간의 제휴를 통해 상생하는 비즈니스 참여를 기다립니다.</h4>
+                    </div>
+                    <Image src="/images/img_mail_cont.svg" width={300} height={300} alt="contact-이미지" />
+                </div>
+                <div className="right-box">
                     <label>문의</label>
-                    <input />
+                    <select>
+                        <option value="">문의유형을 선택해주세요.</option>
+                        <option value="꿀트립 서비스">꿀트립 서비스</option>
+                        <option value="메타버스 에이전시">메타버스 에이전시</option>
+                        <option value="기타">기타</option>
+                    </select>
                     <label>이름</label>
                     <input />
+                    <label>이메일</label>
+                    <div className="email-box">
+                        <input /> @ <input />
+                    </div>
+                    <label>연락처</label>
+                    <div className="phone-box">
+                        <input /> - <input /> - <input />
+                    </div>
+                    <label>제목 </label>
+                    <input />
+                    <label>문의내용</label>
+                    <textarea />
                 </div>
-            </form>
-            <div className="btn-list">
-                <button>취소</button>
-                <button onClick={() => sendEmailHandler()}>확인</button>
             </div>
+            <div className="check-wrap">
+                <div className="check-box">
+                    <input type="checkbox" />
+                    개인정보 수집,이용 동의 <span>내용보기</span>
+                </div>
+                <div className="btn-list">
+                    <button>취소</button>
+                    <button onClick={() => sendEmailHandler()}>확인</button>
+                </div>
+            </div>
+
             <style jsx>{`
                 section {
                     width: 100vw;
                     display: flex;
                     flex-direction: column;
                     align-items: center;
-                    padding-top: 136px;
+                    padding-top: 146px;
                 }
-                .title {
+                .left-box {
+                    margin-right: 32px;
+                }
+                .contact-wrap {
+                    display: flex;
+                    justify-content: center;
+                    margin-bottom: 22px;
+                }
+                .check-wrap {
                     display: flex;
                     flex-direction: column;
-                    justify-content: center;
                     align-items: center;
-                    margin-bottom: 80px;
                 }
                 h1 {
                     font-weight: var(--bold);
-                    font-size: 48px;
+                    font-size: 32px;
                     line-height: 40px;
-                    letter-spacing: -0.015em;
+                    display: flex;
                     color: var(--font_400);
+                }
+                select {
+                    padding: 8px;
+                    width: 328px;
+                    height: 42px;
+                    background: #ffffff;
+                    border: 1px solid #dddddd;
+                    color: var(--font_100);
+                }
+                h4 {
+                    font-weight: 500;
+                    font-size: 16px;
+                    line-height: 20px;
+                    color: #000000;
+                }
+
+                .title {
                     margin-bottom: 16px;
                 }
-                .pc-form {
-                    box-sizing: border-box;
+                .sub-title {
+                    width: 350px;
+                    margin-bottom: 260px;
+                }
+                .right-box {
                     display: flex;
                     flex-direction: column;
-                    align-items: center;
-                    width: 1000px;
-                    height: 498px;
-                    background: #fafafa;
-                    border-width: 1px 0px;
-                    border-style: solid;
-                    border-color: #dddddd;
-                    margin-bottom: 32px;
-                    padding: 20px 0 20px 0;
+                    justify-content: flex-start;
                 }
-                .btn-list {
+
+                textarea {
+                    box-sizing: border-box;
                     display: flex;
-                    justify-content: center;
+                    flex-direction: row;
+                    align-items: flex-start;
+                    padding: 8px;
+                    gap: 10px;
+                    width: 588px;
+                    height: 116px;
+                    background: #ffffff;
+                    border: 1px solid #dddddd;
+                    flex: none;
+                    order: 1;
+                    flex-grow: 0;
                 }
+                label {
+                    font-weight: 500;
+                    font-size: 16px;
+                    line-height: 20px;
+                    color: #404040;
+                    margin: 14px 0 4px 0;
+                }
+                option {
+                    font-weight: 400;
+                    font-size: 16px;
+                    line-height: 20px;
+                    display: flex;
+                    color: #767676;
+                }
+                .email-box > input {
+                    width: 200px;
+                }
+                .phone-box > input {
+                    width: 100px;
+                }
+                .check-box {
+                    display: flex;
+                }
+                .check-box > input {
+                    width: 16px;
+                    margin-right: 8px;
+                }
+
+                .btn-list {
+                    margin-top: 16px;
+                }
+                input {
+                    box-sizing: border-box;
+                    padding: 8px;
+                    background: #ffffff;
+                    border: 1px solid #dddddd;
+                    width: 328px;
+                    border-radius: 5px;
+                }
+
                 button {
                     width: 190px;
                     height: 56px;
@@ -80,7 +180,6 @@ export default function ContactPage({ children }) {
                     color: #ff8f0b;
                     margin: 0 20px 80px 20px;
                 }
-
                 button:hover {
                     background: #ff8f0b;
                     border: 1px solid #ff8f0b;
--- 4요망진연구소/ymjlabpage/homepage/section_kkultrip/Mainpage.js
+++ 4요망진연구소/ymjlabpage/homepage/section_kkultrip/Mainpage.js
@@ -1,74 +1,77 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { navVariants } from "../utils/motion";
-import Link from "next/link";
+import { staggerContainer } from "../utils/motion";
 import Image from "next/image";
-import styles from "./Mainpage.module.css";
 import kkultrip_main from "../public/images/kkultrip_img/img_b_bg_kt.svg";
+import { TypingText } from "../components";
 
 const Mainpage = () => {
     return (
-        <section>
-            <div className="main-img-box">
-                <Image src={kkultrip_main} alt="꿀트립 메인이미지" width={800} height={1000} />
-            </div>
-            <div className="title">
-                <h1>꿀 같은 여행</h1>
-                <h1>더 달콤하게 누리세요!</h1>
-                <p>신경쓰지 않아도 할인받은 꿀같은 여행앱</p>
-                <div className="width-div">&nbsp;</div>
-            </div>
-            <style jsx>{`
-                section {
-                    width: 100vw;
-                    height: var(--page_height);
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    background: #fff8dd;
-                }
-                .main-img-box {
-                    position: relative;
-                    margin-top: 200px;
-                }
-                .title {
-                    display: flex;
-                    flex-direction: column;
-                    position: absolute;
-                    top: 100px;
-                    z-index: 10;
-                }
-                .width-div {
-                    width: 900px;
-                }
+        <motion.div className="section-box-col" variants={staggerContainer} initial="hidden" whileInView="show" viewport={{ once: true, amount: 0.25 }}>
+            <section>
+                <div className="main-img-box">
+                    <Image src={kkultrip_main} alt="꿀트립 메인이미지" width={800} height={1000} />
+                </div>
+                <div className="title">
+                    <h1>꿀 같은 여행</h1>
+                    <h1>더 달콤하게 누리세요!</h1>
+                    <p>
+                        <TypingText title="신경쓰지 않아도 할인받은 꿀같은 여행앱" />
+                    </p>
+                    <div className="width-div">&nbsp;</div>
+                </div>
+                <style jsx>{`
+                    section {
+                        width: 100vw;
+                        height: var(--page_height);
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        /* background: #fff8dd; */
+                    }
+                    .main-img-box {
+                        position: relative;
+                        margin-top: 200px;
+                    }
+                    .title {
+                        display: flex;
+                        flex-direction: column;
+                        position: absolute;
+                        top: 100px;
+                        z-index: 10;
+                    }
+                    .width-div {
+                        width: 900px;
+                    }
 
-                h1 {
-                    font-family: "Gmarket Sans";
-                    font-style: normal;
-                    font-weight: 800;
-                    font-size: 60px;
-                    line-height: 40px;
-                    letter-spacing: -0.015em;
-                    color: var(--font_500);
-                    margin-bottom: 16px;
-                    font-stretch: expanded;
-                }
-                p {
-                    font-family: "Gmarket Sans";
-                    font-style: normal;
-                    font-weight: 400;
-                    font-size: 19px;
-                    line-height: 19px;
-                    display: flex;
-                    align-items: center;
-                    color: #181818;
-                }
+                    h1 {
+                        font-family: "Gmarket Sans";
+                        font-style: normal;
+                        font-weight: 800;
+                        font-size: 60px;
+                        line-height: 40px;
+                        letter-spacing: -0.015em;
+                        color: var(--font_500);
+                        margin-bottom: 16px;
+                        font-stretch: expanded;
+                    }
+                    p {
+                        font-family: "Gmarket Sans";
+                        font-style: normal;
+                        font-weight: 400;
+                        font-size: 19px;
+                        line-height: 19px;
+                        display: flex;
+                        align-items: center;
+                        color: #181818;
+                    }
 
-                @media only screen and (max-width: 600px) {
-                }
-            `}</style>
-        </section>
+                    @media only screen and (max-width: 600px) {
+                    }
+                `}</style>
+            </section>
+        </motion.div>
     );
 };
 
